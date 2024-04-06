fa365e9fdd27281b566924edc02f08b8faca3928
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-04-05 19:23:40 -0500

    Reformat text_to_textnodes to be more readable

--- src/textnode.py
+++ src/textnode.py
@@ -109,4 +109,10 @@ def split_nodes_link(old_nodes: list[TextNode]):
     return new_nodes
 
 def text_to_textnodes(text: str):
-    return split_nodes_link(split_nodes_image(split_nodes_delimiter(split_nodes_delimiter(split_nodes_delimiter([TextNode(text, text_type_text)], "**", text_type_bold), "*", text_type_italic), "`", text_type_code)))
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
