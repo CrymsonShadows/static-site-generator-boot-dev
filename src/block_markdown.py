9b767c882b542d43c484a74cbea7c33cc8ba9a96
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-04-08 16:24:45 -0500

    Change markdown_to_blocks to not excessively convert map to list

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,6 @@
 def markdown_to_blocks(markdown: str):
     lines = markdown.split("\n\n")
     blocks: list[str] = []
-    blocks = list(map(lambda line: line.strip(), lines))
+    blocks = map(lambda line: line.strip(), lines)
     blocks = list(filter(lambda block: not block == "", blocks))
     return blocks
