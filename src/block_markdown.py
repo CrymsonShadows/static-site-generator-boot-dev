323479b13e7b0c12618ef550da7e71b9d8132652
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-04-09 18:30:14 -0500

    Implement and test block_to_block_type method

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,49 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown: str):
     lines = markdown.split("\n\n")
     blocks: list[str] = []
     blocks = map(lambda line: line.strip(), lines)
     blocks = list(filter(lambda block: not block == "", blocks))
     return blocks
+
+def block_to_block_type(block: str):
+    # Heading
+    if block.startswith(("#", "##", "###", "####", "#####", "######")):
+        return block_type_heading
+    # Code
+    if block.startswith("```") and block.endswith("```") and len(block) != 3:
+        return block_type_code
+    lines: list[str] = block.split("\n")
+    # Quote
+    every_line_char_start = True
+    for line in lines:
+        if line.startswith(">") != True:
+            every_line_char_start = False
+            break
+    if every_line_char_start == True:
+        return block_type_quote
+    # Unordered List
+    every_line_char_start = True
+    for line in lines:
+        if line.startswith("*") != True and line.startswith("-") != True:
+            every_line_char_start = False
+            break
+    if every_line_char_start == True:
+        return block_type_unordered_list
+    # Ordered List
+    num = 1
+    every_line_char_start = True
+    for line in lines:
+        if line.startswith(f"{num}.") != True:
+            every_line_char_start = False
+            break
+        num += 1
+    if every_line_char_start == True:
+        return block_type_ordered_list
+    return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, block_type_paragraph, block_type_heading, block_type_code, block_type_quote, block_type_unordered_list, block_type_ordered_list
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks_empty(self):
@@ -27,6 +27,77 @@ class TestBlockMarkdown(unittest.TestCase):
             ]
         )
 
+    def test_block_to_block_type_paragraph_block(self):
+        self.assertEqual(
+            block_to_block_type("This is a paragraph.\nThis is the second line of the paragraph."),
+            block_type_paragraph
+        )
+
+    def test_block_to_block_type_heading_1(self):
+        self.assertEqual(
+            block_to_block_type("# This is a heading"),
+            block_type_heading
+        )
+
+    def test_block_to_block_type_heading_2(self):
+        self.assertEqual(
+            block_to_block_type("## This is a subheading"),
+            block_type_heading
+        )
+
+    def test_block_to_block_type_code(self):
+        self.assertEqual(
+            block_to_block_type("```This is code```"),
+            block_type_code
+        )
+
+    def test_block_to_block_type_code_no_trailing_backticks(self):
+        self.assertEqual(
+            block_to_block_type("```This code block doesn't end with backticks"),
+            block_type_paragraph
+        )
+
+    def test_block_to_block_type_code_no_starting_backticks(self):
+        self.assertEqual(
+            block_to_block_type("This code block has no starting backticks```"),
+            block_type_paragraph
+        )
+
+    def test_block_to_block_type_quote(self):
+        self.assertEqual(
+            block_to_block_type("> This the first line of a quote\n> This is the second line of a quote"),
+            block_type_quote
+        )
+    
+    def test_block_to_block_type_quote_only_one_line_with_arrow(self):
+        self.assertEqual(
+            block_to_block_type("> This is the first line of the quote\nThis is the second line without the arrow"),
+            block_type_paragraph
+        )
+
+    def test_block_to_block_type_unordered_list(self):
+        self.assertEqual(
+            block_to_block_type("* This is the first item on the list\n- This is the second item"),
+            block_type_unordered_list
+        )
+
+    def test_block_to_block_type_unordered_list(self):
+        self.assertEqual(
+            block_to_block_type("* First item\nSecond line"),
+            block_type_paragraph
+        )
+
+    def test_block_to_block_type_ordered_list(self):
+        self.assertEqual(
+            block_to_block_type("1. First item\n2. Second Item\n3. Third item"),
+            block_type_ordered_list
+        )
+
+    def test_block_to_block_type_odered_list_out_of_order_lines(self):
+        self.assertEqual(
+            block_to_block_type("1. First item\n3.Second item"),
+            block_type_paragraph
+        )
 
 if __name__ == "__main__":
     unittest.main()
