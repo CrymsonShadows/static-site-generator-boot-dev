ef06f57e922c667ab4c102205d6b48238790e441
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-06-03 07:42:09 -0500

    Trim copy_dir

--- src/copy_static.py
+++ src/copy_static.py
@@ -3,19 +3,18 @@ from os import listdir, mkdir
 from shutil import copy, rmtree
 
 def copy_dir(src: str, dest: str):
+    if not exists(dest):
+        mkdir(dest)
+        print(f"{dest} is created")
+
     dir_list: list[str] = listdir(src)
     print(dir_list)
     for dir in dir_list:
         if isfile(join(src, dir)):
-            if not exists(join(dest, dir)):
-                print(f"{join(src, dir)} is a file and copied to {join(dest, dir)}")
-                copy(join(src, dir), join(dest, dir))
+            print(f" * {join(src, dir)} -> {join(dest, dir)}")
+            copy(join(src, dir), join(dest, dir))
         else:
-            print(f"{join(src, dir)} is not a file")
-            if not exists(join(dest, dir)):
-                mkdir(join(dest, dir))
-                print(f"{join(dest, dir)} is created")
-                copy_dir(src=join(src, dir), dest=join(dest, dir))
+            copy_dir(src=join(src, dir), dest=join(dest, dir))
 
 def copy_static():
     if exists("public"):
